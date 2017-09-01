355a191d3d28806c8efddd1429812410f1e77278
Patsy Pineda <patsyp014@gmail.com>
2017-09-01 00:26:55 -0500

    finished new algo

--- JavaScript/other.js
+++ JavaScript/other.js
@@ -1,6 +1,7 @@
 //Write a recursive function that given a number returns sum of integers from 1 to that number.
 
 function rSigma(n){
+	n = Math.floor(n)
 	var sum = 0
 	if (n == 1){
 		return 0
@@ -8,12 +9,19 @@ function rSigma(n){
 	if (n > 1){
 		while (n >= 1){
 			sum +=n
+			console.log(sum)
 			n--
-			console.log(n)
+		}
+		return sum
+	}
+	if (n < 1){
+		while (n <=1){
+			sum += n
+			n++
 		}
 		return sum
 	}
 }
 
-console.log(rSigma(2.5)) //should return 15 (1+2+3+4+5)
+console.log(rSigma(5)) //should return 15 (1+2+3+4+5)
 
