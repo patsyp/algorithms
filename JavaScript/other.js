aaf2cbf015db569a1609624d84543001dbd170a5
Patsy Pineda <patsyp014@gmail.com>
2017-09-05 23:35:59 -0500

    almost done

--- JavaScript/other.js
+++ JavaScript/other.js
@@ -22,7 +22,18 @@ function rSigma(n){
 		return sum
 	}
 }
-
 console.log(rSigma(5)) //should return 15 (1+2+3+4+5)
 
+//Return whether a sorted array contains a value but without sequentially iterating through array.
+function binarySearch(arr, val){
+	var len = arr.length
+	var half = Math.floor(len/2)
+	if (arr[half]<val){ 
+		var lowerHalf= arr.slice(half, len)
+	}
+	if (arr[half]>val){
+		var upperHalf= arr.slice(0, half)
+	}
+}
 
+console.log(binarySearch([1, 2, 4, 5, 7, 9,10], 9))
